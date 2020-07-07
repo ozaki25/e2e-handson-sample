1bcce0c8daeb4a69d34939d413cec1ad496531ce
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-07-08 01:05:36 +0900

    add screenshot

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   await page.goto('https://google.com');
+  await page.screenshot({ path: '1.png', fullPage: true });
+
   await page.type('input[name="q"]', 'puppeteer');
+  await page.screenshot({ path: '2.png', fullPage: true });
+
   await page.click('input[name="btnK"]');
   await page.waitForSelector('#search');
+  await page.screenshot({ path: '3.png', fullPage: true });
+
   await page.click('a[href="https://pptr.dev/"]');
   await page.waitForSelector('content-box');
+  await page.screenshot({ path: '4.png', fullPage: true });
 
   await browser.close();
 })();
