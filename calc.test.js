05ca9cc326d7952526e6c29cf84a5d3bccb18afe
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-07-08 11:14:04 +0900

    add test sample

--- calc.test.js
+++ calc.test.js
@@ -1,7 +1,20 @@
-const { add } = require('./calc');
+const { add, subtract } = require('./calc');
 
-describe('足し算のテスト', () => {
-  test('1と2を渡すと3が返ること', () => {
-    expect(add(1, 2)).toBe(3);
+describe('calcのテスト', () => {
+  describe('#add', () => {
+    test('1と2を渡すと3が返ること', () => {
+      expect(add(1, 2)).toBe(3);
+    });
+    test('-1と-2を渡すと-3が返ること', () => {
+      expect(add(-1, -2)).toBe(-3);
+    });
+  });
+  describe('#subtract', () => {
+    test('2と1を渡すと1が返ること', () => {
+      expect(subtract(2, 1)).toBe(1);
+    });
+    test('1と2を渡すと-1が返ること', () => {
+      expect(subtract(1, 2)).toBe(-1);
+    });
   });
 });
