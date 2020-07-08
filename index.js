15fc710ad682dfb37928f0fc540173eb38a8410d
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-07-08 14:06:15 +0900

    update pupeteer sample

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 300 });
+  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch({ headless: false, slowMo: 300 });
   const page = await browser.newPage();
 
   await page.goto('https://google.com');
@@ -10,12 +11,16 @@ const puppeteer = require('puppeteer');
   await page.type('input[name="q"]', 'puppeteer');
   await page.screenshot({ path: '2.png', fullPage: true });
 
-  await page.click('input[name="btnK"]');
-  await page.waitForSelector('#search');
+  await Promise.all([
+    page.click('input[name="btnK"]'),
+    page.waitForNavigation(),
+  ]);
   await page.screenshot({ path: '3.png', fullPage: true });
 
-  await page.click('a[href="https://pptr.dev/"]');
-  await page.waitForSelector('content-box');
+  await Promise.all([
+    page.click('a[href="https://github.com/puppeteer/puppeteer"]'),
+    page.waitForNavigation(),
+  ]);
   await page.screenshot({ path: '4.png', fullPage: true });
 
   await browser.close();
