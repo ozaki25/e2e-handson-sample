1c05919dc06463c0be9c733b0d877ce945004700
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-07-10 01:03:50 +0900

    modify screenshot dir

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   await page.goto('https://google.com');
-  await page.screenshot({ path: '1.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/1.png', fullPage: true });
 
-  await page.type('input[name="q"]', 'puppeteer');
-  await page.screenshot({ path: '2.png', fullPage: true });
+  await page.type('input[name="q"]', 'puppeteerr');
+  await page.screenshot({ path: 'screenshots/2.png', fullPage: true });
 
   await Promise.all([
     page.click('input[name="btnK"]'),
     page.waitForNavigation(),
   ]);
-  await page.screenshot({ path: '3.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/3.png', fullPage: true });
 
   await Promise.all([
     page.click('a[href="https://github.com/puppeteer/puppeteer"]'),
     page.waitForNavigation(),
   ]);
-  await page.screenshot({ path: '4.png', fullPage: true });
+  await page.screenshot({ path: 'screenshots/4.png', fullPage: true });
 
   await browser.close();
 })();
